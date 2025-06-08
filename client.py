6380b4113cb7af272d8253cd895389e193f0c084
Le Suan Bach <147260637+IT23-2@users.noreply.github.com>
2025-06-08 19:21:11 +0300

    Letter

--- client.py
+++ client.py
@@ -1,18 +1,43 @@
 import socket
 import pickle
+from Crypto.PublicKey import RSAAdd commentMore actions
+from Crypto.Cipher import PKCS1_OAEP
 
 HOST = '127.0.0.1'
 PORT = 8080
 
+# Diffie-Hellman parametersAdd commentMore actions
+p, g, a = 7, 5, 3
+A = g ** a % p
+
+# Connect to server
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
-A = g ** a % p
+# Send Diffie-Hellman parameters and A to the server
 sock.send(pickle.dumps((p, g, A)))
+
+# Receive B from the server
 msg = sock.recv(1024)Add commentMore actions
 B = pickle.loads(msg)
-K = B ** a % pAdd commentMore actions
-print("K =", K)
+
+# Calculate the shared secret K
+K = B ** a % p
+print("Shared secret K =", K)Add commentMore actions
+
+# Receive the server's public key (PEM format)
+msg = sock.recv(1024)Add commentMore actions
+public_key_pem = pickle.loads(msg)
+
+# Import the public key from PEM format
+server_public_key = RSA.import_key(public_key_pem)
+
+# Encrypt a message using the server's public key
+message = "Hello, server!"
+encryptor = PKCS1_OAEP.new(server_public_key)
+encrypted_message = encryptor.encrypt(message.encode())
+
+# Send the encrypted message to the server
+sock.send(pickle.dumps(encrypted_message))
 
 sock.close()
