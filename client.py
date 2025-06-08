bc49e4d2a6bb255f68fff9c923e273a0c6984d29
Le Suan Bach <147260637+IT23-2@users.noreply.github.com>
2025-06-08 19:15:27 +0300

    calc

--- client.py
+++ client.py
@@ -10,5 +10,9 @@ sock.connect((HOST, PORT))
 p, g, a = 7, 5, 3
 A = g ** a % p
 sock.send(pickle.dumps((p, g, A)))
+msg = sock.recv(1024)Add commentMore actions
+B = pickle.loads(msg)
+K = B ** a % pAdd commentMore actions
+print("K =", K)
 
 sock.close()
